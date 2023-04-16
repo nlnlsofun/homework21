60b0b95d3920ae30ee9d84e64316d7461e6561bc
柯仁傑 <@DESKTOP-34DDU9R>
2023-04-16 22:23:03 +0800

    加入專案檔案。

---
+++ homework21/homework21.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <iomanip> 
+#include <limits>  
+
+int main() {
+    const int N = 10;
+    float input[N];
+    float max = std::numeric_limits<float>::min();
+    float min = std::numeric_limits<float>::max();
+
+
+    for (int i = 0; i < N; i++) {
+        std::cin >> input[i];
+
+        if (input[i] > max) {
+            max = input[i];
+        }
+        if (input[i] < min) {
+            min = input[i];
+        }
+    }
+
+
+    std::cout << std::fixed << std::setprecision(2) << max << " " << min << std::endl;
+
+    return 0;
+}
